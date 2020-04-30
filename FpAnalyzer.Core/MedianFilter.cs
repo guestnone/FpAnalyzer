00396d41e3143e11729799cc315e9f727673ea2f
Patrick Rećko <guest_none@protonmail.com>
2020-04-30 15:15:47 +0200

    WIP: Skeleton

---
+++ FpAnalyzer.Core/MedianFilter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FpAnalyzer.Core
+{
+    class MedianFilter
+    {
+        // Source for inspiration: https://softwarebydefault.com/2013/05/18/image-median-filter/
+    }
+}
